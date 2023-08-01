050efe16fc0c0a0e3d4f5f2671f5ee6ba749af31
Samyak Jain <139710951+samyakj07@users.noreply.github.com>
2023-08-02 01:25:59 +0530

    Add files via upload

---
+++ Task 2.py
@@ -0,0 +1,20 @@
+# Task 2: Python Code to send Email to everyone use module behind it
+
+
+import smtplib, ssl
+
+port = 465  # For SSL
+smtp_server = "smtp.gmail.com"
+sender_email = "jainsamyak170@gmail.com"  # Enter your address
+receiver_email = "31samyak@gmail.com"  # Enter receiver address
+password = "rjtr dejn drrl ggfp"  # Replace this with the actual password for sender_email
+message = "What is up man"
+Subject: "Hi there"
+
+# This message is sent from Python.
+# IM SAMYAK
+
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, message)
